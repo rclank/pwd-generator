a8e81e38bed1dfd8df2681c3bfb899d25a24f7b2
rclank <53981655+rclank@users.noreply.github.com>
2021-10-24 17:25:11 -0400

    good progress

--- Develop/script.js
+++ Develop/script.js
@@ -3,25 +3,62 @@ let getLength = function() {
   const minLength = 8;
   const maxLength = 128;
 
+
+  // prompt for length
   let promptLength = window.prompt(`Please choose a password length from ${minLength}-${maxLength} characters (inclusive).`);
 
+  // potential enhancement: terminate function if cancel (null)?
+
+  // validate length input
   if (!(promptLength >= 8 && promptLength <= 128)) {
-    
+    window.alert(`You need to provide a valid length from ${minLength}-${maxLength} characters (inclusive)! Please try again.`);
+    return getLength();
   }
+  return parseInt(promptLength);
+}
+
+let getCharCriteria = function() {
+  let charCriteriaObj = {};
+
+  charCriteriaObj.lowerChars = window.confirm(`Would you like to include lowercase characters?`);
+  charCriteriaObj.upperChars = window.confirm(`Would you like to include uppercase characters?`);
+  charCriteriaObj.numbers = window.confirm(`Would you like to include numbers?`);
+  charCriteriaObj.specialChars = window.confirm(`Would you like to include special characters?`);
+
+  // check if at least one value is true. not extensible
+  if (!(charCriteriaObj.lowerChars || charCriteriaObj.upperChars || charCriteriaObj.numbers || charCriteriaObj.specialChars)) {
+    window.alert('Please specify at least 1 criteria!');
+    return getCharCriteria();
+  }
+  // enhancement: better way to check if at least one is true
+
+  return charCriteriaObj;
 }
 
 let getCriteria = function() {
-  let length = getLength();
+  let criteriaObj = {};
 
+  criteriaObj.length = getLength();
+
+  // break out character criteria to check that at least one is true
+  let charCriteria = getCharCriteria();
+
+  // concat critriaObj and charCriteria
+  criteriaObj = Object.assign(criteriaObj, charCriteria);
+
+  return(criteriaObj);
 }
 
+let getCharacters = function(criteriaObject) {
+  const lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+  const upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+}
 
 
 function generatePassword() {
   let criteria = getCriteria();
 
-
-  console.log(promptLength, typeof(promptLength));
+  let characters = getCharacters();
 
 
   return password;
